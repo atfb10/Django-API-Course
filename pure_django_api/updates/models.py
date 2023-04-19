08c9d599ca20bf1033bcd536f9e33123d5ccf73e
atfb84 <atfb84@mst.edu>
2023-04-19 15:59:01 -0700

    Create updates app & update model

---
+++ pure_django_api/updates/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+def upload_update_image(instance, filename):
+    return f'updates/{instance.user}{filename}'
+
+# Create your models here.
+class Update(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.content or ""
