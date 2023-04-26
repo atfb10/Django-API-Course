409da3f6c4c96b7d3b77520ba7353b1d01372300
atfb84 <atfb84@mst.edu>
2023-04-26 14:18:27 -0700

    validate serializers

--- Django_REST_Framework/REST_Framework/status/api/serializers.py
+++ Django_REST_Framework/REST_Framework/status/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from status.models import Status
 
+# NOTE: THIS IS JUST AN EXAMPLE OF CREATING A CUSTOM SERIALIZER that is not using a model. 
+class CustomerSerializer(serializers.Serializer):
+    content = serializers.CharField()
+    email = serializers.EmailField()
+    
 class StatusSerializer(serializers.ModelSerializer):
     '''
     turn data into JSON and validate data
@@ -12,3 +17,24 @@ class StatusSerializer(serializers.ModelSerializer):
             'content',
             'image'
         ]
+
+    '''
+    field level validation
+    NOTE: just need to name validate_<fieldname>
+    '''
+    def validate_content(self, value):
+        if len(value) > 10000:
+            raise serializers.ValidationError("Content is wayyyy too long")
+        return value
+    
+    '''
+    entire data validation
+    '''
+    def validate(self, data):
+        content = data.get('content', None)
+        if content == "":
+            content = None
+        img = data.get('image', None)
+        if content == None and img == None:
+            raise serializers.ValidationError("Content OR image must be present!")
+        return data
