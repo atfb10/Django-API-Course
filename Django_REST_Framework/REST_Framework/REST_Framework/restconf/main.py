bbc32ee8a13aebb51c55a8207fd5aa076cf59da9
atfb84 <atfb84@mst.edu>
2023-05-15 16:06:01 -0700

    JWT settings

--- Django_REST_Framework/REST_Framework/REST_Framework/restconf/main.py
+++ Django_REST_Framework/REST_Framework/REST_Framework/restconf/main.py
@@ -1,4 +1,5 @@
 # Set up having default permission_classes & default authentication classes
+import datetime
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
     # 'rest_framework.authentication.BasicAuthentication',
@@ -8,6 +9,32 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
     'rest_framework.permissions.IsAuthenticated'
     )
+}
+
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+
 }
 
 # NOTE: Super cool! Now, if I want the default authentication or permission classes above, I do not have to define them in the API views!
