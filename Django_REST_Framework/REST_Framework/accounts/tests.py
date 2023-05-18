77d6625261afba277d924c340af9419206de0482
atfb84 <atfb84@mst.edu>
2023-05-18 13:31:30 -0700

    Make an automated unittest

--- Django_REST_Framework/REST_Framework/accounts/tests.py
+++ Django_REST_Framework/REST_Framework/accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self) -> None:
+        user = User.objects.create(username='cfe', email='hi@gmail.com')
+        user.set_password('123')
+        user.save()
+        return 
+    
+    def test_create_user(self):
+        qs = User.objects.filter(username='cfe')
+        self.assertEqual(qs.count(), 1)
